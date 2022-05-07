565424c8116d24427ba5210cfc1c7e837186b312
shamalloke <100463701+shamalloke@users.noreply.github.com>
2022-05-07 17:19:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+function setup(){
+  canvas= createCanvas(300,300);
+    background("#000");
+    canvas.center();
+    synth=window.speechSynthesis;
+    canvas.mouseReleased(simplify_canvas);
+}
+
+function clearCanvas(){
+    background("#000");
+}
+
+function preload(){
+  classifier=ml5.Imageclassifier("DoodleNet");
+
+}
+
+function simplify_canvas(){
+  classifier.classify(canvas,gotResult);
+}
+
+function gotResult(error,result){
+  if (error) {
+    (console.error(error));
+  }
+  else {
+    console.log(results);
+  document.getElementById("Label").innerHTML=" Label "+ results[0].label;
+  document.getElementById("Confidence").innerHTML=" Confidence "+ Math.round(results[0].label * 100) +  " %";
+  utterThis= new SpeechSynthesisUtterance(results[0].label);
+  synth.speak(utterThis);
+  }
+
+}
+
+function draw(){
+  strokeweight(10);
+  stroke("white");
+  if(MouseisPressed){
+    line(pmouseX,pmouseY,mouseX,mouseY);
+  }
+}
